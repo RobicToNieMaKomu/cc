1562a5e6d3167d2597695674f7f5c87b7419d4e4
RobicToNieMaKomu <>
2014-04-15 20:32:41 +0200

    rest test

---
+++ src/main/java/com/polmos/cc/rest/RESTApplication.java
@@ -0,0 +1,11 @@
+package com.polmos.cc.rest;
+
+import javax.ws.rs.ApplicationPath;
+
+/**
+ *
+ * @author RobicToNieMaKomu
+ */
+@ApplicationPath("/")
+public class RESTApplication {
+}
---
+++ src/main/java/com/polmos/cc/rest/RESTResources.java
@@ -0,0 +1,24 @@
+package com.polmos.cc.rest;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+/**
+ *
+ * @author RobicToNieMaKomu
+ */
+@Path("/rest")
+@Produces("application/json")
+@Consumes("application/json")
+public class RESTResources {
+
+    @GET
+    @Path("/hello")
+    public JsonObject sayHello() {
+        return Json.createObjectBuilder().add("Hello", "world").build();
+    }
+}
